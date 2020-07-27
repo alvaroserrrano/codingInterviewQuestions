0163237832ac489476584db24af8a6adb322bbca
Alvaro <alvaro.serrano@wmich.edu>
2020-07-27 18:48:29 +0200

    rm_sorted_dups

--- arrays/rm_dups.py
+++ arrays/rm_dups.py
@@ -1,4 +1,19 @@
-def rm_dups(x):
-    return list(dict.fromkeys(x))
-my_list = rm_dups(['a', 'b', 'a', 'c', 'b'])
-print(my_list)
+#Remove duplicates from sorted array
+def rm_dups(arr):
+    i = 0
+    while i < len(arr)-1:
+        if arr[i] == arr[i+1]:
+            del arr[i]
+        else:
+            i += 1
+    return len(arr)
+
+import unittest
+class Test(unittest.TestCase):
+    def test_rm_dups(self):
+        arr = [1, 3, 3, 4, 5]
+        rm_dups(arr)
+        self.assertEqual([1, 3, 4 , 5], arr)
+
+if __name__ == '__main__':
+    unittest.main()
