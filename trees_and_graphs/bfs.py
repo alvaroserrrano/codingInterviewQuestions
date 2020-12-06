e25528ac7a797e82af6aa9428a0639036501c9e2
Alvaro <alvaro.serrano@wmich.edu>
2020-12-06 17:53:05 +0100

    dfs + bfs

---
+++ trees_and_graphs/bfs.py
@@ -0,0 +1,21 @@
+#Depth First Search
+
+#Queue implementation
+def bfs(graph, start):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+def bfs_path(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
---
+++ trees_and_graphs/dfs.py
@@ -0,0 +1,31 @@
+#Depth First Search
+
+# Stack implemenetation
+def dfsStack(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+# Recursive implementation
+def dfsRecursive(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                stack.append((next, path + [next]))
+
